6d442697b7e933240bfe66bdd7c71fc0471a309c
osmancan23 <osmacan2319072@gmail.com>
2024-04-03 16:28:58 +0300

    created app structure

---
+++ Location-Search/View/Place/PlaceDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PlaceDetailViewController.swift
+//  Location-Search
+//
+//  Created by Osmancan Akagündüz on 3.04.2024.
+//
+
+import UIKit
+
+class PlaceDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Location-Search/View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Location-Search
+//
+//  Created by Osmancan Akagündüz on 2.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

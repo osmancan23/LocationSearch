167b0068739c937417b69603e667d3c8c6559844
osmancan23 <osmacan2319072@gmail.com>
2024-05-02 14:55:50 +0300

    edited map and search view

--- Location-Search/View/Map/MapViewController.swift
+++ Location-Search/View/Map/MapViewController.swift
@@ -6,24 +6,149 @@
 //
 
 import UIKit
-
+import MapKit
+import GoogleMaps
+import CoreLocation
+import Combine
 class MapViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    var polylines: [GMSPolyline] = []
+    var markers: [GMSMarker] = []
 
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var googleMapsView: UIView!
+    var locationManager = CLLocationManager()
+    var latitude: CLLocationDegrees?
+    var longitude: CLLocationDegrees?
+    var mapView : GMSMapView?
+    var zoom: Float = 15
+      
+    
+    private var cancelable : Set<AnyCancellable> = []
+    private let coreLocationManager : CoreLocationManager = CoreLocationManager.instance
+    
+    private let textField : UITextField = {
+        let textField = UITextField(frame: CGRect(x: 0, y: 0, width: 100, height: 30))
+        textField.backgroundColor = .white
+        textField.borderStyle = .roundedRect
+        textField.placeholder = "Metin girin..."
+        
+        return textField
+    }()
+    
+    override func viewDidLoad() {
+       super.viewDidLoad()
+       locationManager.delegate = self
+       locationManager.requestWhenInUseAuthorization()
+    
     }
     
+      @IBAction func zoomIn(_ sender: Any) {
+          zoom += 1
+          mapView?.animate(toZoom: zoom)
+      }
+      
+      @IBAction func zoomOut(_ sender: Any) {
+          zoom -= 1
+          mapView?.animate(toZoom: zoom)
+      }
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension MapViewController : UISearchBarDelegate {
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        print("")
+        self.navigationController?.pushViewController(SearchViewController(), animated: true)
     }
-    */
+}
 
+extension MapViewController : CLLocationManagerDelegate {
+    
+   
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        if let location = locations.last{
+            latitude = location.coordinate.latitude
+            longitude = location.coordinate.longitude
+            buildMapView()
+           
+            buildMarker(position: CLLocationCoordinate2D(latitude: latitude!, longitude: longitude!), title: "Me")
+            
+           
+      }
+    }
+    
+   
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .authorizedWhenInUse, .authorizedAlways:
+            locationManager.startUpdatingLocation()
+        default:
+            print("location permission denied")
+        }
+    }
+ 
+    func buildMapView()  {
+        removeAllMarkers()
+        //MARK: - Crating Google Maps
+        let camera = GMSCameraPosition.camera(withLatitude:  latitude!, longitude: longitude!, zoom: zoom)
+        mapView = GMSMapView.map(withFrame: self.googleMapsView.bounds, camera: camera)
+        mapView?.isMyLocationEnabled = true
+        mapView?.settings.myLocationButton = true
+        mapView?.settings.rotateGestures = true
+        mapView?.settings.zoomGestures = true
+        if let mapView = mapView {
+              googleMapsView.addSubview(mapView)
+        }
+        
+    }
+    
+    
+    
+    func addPolyLineWithEncodedStringInMap(encodedString: String) {
+        for polyline in polylines {
+            polyline.map = nil
+        }
+        polylines.removeAll()
+
+        let path = GMSMutablePath(fromEncodedPath: encodedString)
+        //print(path)
+        
+        let polyLine = GMSPolyline(path: path)
+        polyLine.strokeWidth = 5
+        polyLine.strokeColor = .red
+        polyLine.map = mapView
+        
+        polylines.append(polyLine)
+        
+        let mapBounds = GMSCoordinateBounds(path: path!)
+        let cameraUpdate = GMSCameraUpdate.fit(mapBounds)
+        self.mapView?.moveCamera(cameraUpdate)
+        
+
+        }
+    
+    func buildMarker(position: CLLocationCoordinate2D , title: String) {
+        let marker = GMSMarker()
+        marker.position = position
+        marker.title = title
+        marker.map = mapView
+        
+        markers.append(marker)
+    }
+
+    func removeAllMarkers(){
+        for marker in markers{
+            marker.map = nil
+        }
+        markers.removeAll()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+      print("Error at " +  #function + ", Error code: \(error)")
+    }
+    
 }
--- Location-Search/View/Search/SearchViewController.swift
+++ Location-Search/View/Search/SearchViewController.swift
@@ -2,172 +2,29 @@ import UIKit
 import GoogleMaps
 import GooglePlaces
 
-protocol SearchViewControllerDelegate: AnyObject {
-    func didTapPlaceWithCoordinate(with coordinates: CLLocationCoordinate2D)
-}
-
-private var reuseIdentifier = "cell"
 class SearchViewController: UIViewController {
-    // MARK: - Properties
-    let headerViewHeight: CGFloat = 150 // Header view height
-    let cellHeight: CGFloat = 100 // Height of each cell in the table view
     
-    private var places: [Place] = []
-    private var history : [Place] = []
-    weak var delegate : SearchViewControllerDelegate?
+    @IBOutlet weak var menuTableView: UITableView!
     
-    let tableView: UITableView = {
-        let tableView = UITableView()
-        tableView.backgroundColor = .systemBackground
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        return tableView
-    }()
     
-    let headerView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .systemBackground
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
-    let separatorView : UIView = {
-        let separatorView = UIView()
-        separatorView.backgroundColor = .lightGray // Adjust color as desired
-        separatorView.translatesAutoresizingMaskIntoConstraints = false
-        return separatorView
-    }()
-   
+    let data = [[SearchMenuModel(name: "Konumunuz", image: "location"),SearchMenuModel(name: "Haritadan seç", image: "rectangle.and.hand.point.up.left"),], [SearchMenuModel(name: "İstanbul Söğütlüçeşme", image: "clock"),SearchMenuModel(name: "İstanbul Söğütlüçeşme", image: "clock"),SearchMenuModel(name: "İstanbul Söğütlüçeşme", image: "clock"),SearchMenuModel(name: "İstanbul Söğütlüçeşme", image: "clock"),SearchMenuModel(name: "İstanbul Söğütlüçeşme", image: "clock"),]]
     
-    let searchBar: UISearchBar = {
-        let searchBar = UISearchBar()
-        searchBar.barTintColor = UIColor.systemGray
-        searchBar.translatesAutoresizingMaskIntoConstraints = false
-        return searchBar
-    }()
     
-    let  headerImageView = {
-        let imageView = UIImageView(image: UIImage(systemName: "location.circle"))
-        imageView.contentMode = .scaleAspectFit // Maintain aspect ratio
-        imageView.tintColor = .black
-        imageView.frame = CGRect(x: 20, y: 90, width: 38, height: 33) // Adjust width and height for desired size
-        return imageView
-    }()
-    lazy var currentLocationLabel = {
-        let label = UILabel()
-        label.text = "Header Label"
-        label.textColor = .black
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
     
-    // MARK: - Lifecycle
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        style()
-        layout()
-        searchBar.delegate = self
-        tableView.delegate = self
-        tableView.dataSource = self
-        
-
+        menuTableView.dataSource = self
+        menuTableView.delegate = self
+        menuTableView.register(SearchTableViewCell.nib(), forCellReuseIdentifier: SearchTableViewCell.identifier)
+     
     }
-    
-    
-    
+
+  
 }
-// MARK: - Helpers
-extension SearchViewController{
-    private func style(){
-
-        self.navigationItem.titleView = searchBar
-        searchBar.delegate = self
-        view.addSubview(headerView)
-        
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(headerTapped))
-        headerView.addGestureRecognizer(tapGesture)
-        headerView.addSubview(headerImageView)
-        headerView.addSubview(currentLocationLabel)
-
-        tableView.delegate = self
-        tableView.dataSource = self
-        view.addSubview(tableView)
-
-        tableView.register(SearchBarTableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
-        view.addSubview(separatorView)
 
 
-        
-    }
-    private func layout(){
-        headerView.topAnchor.constraint(equalTo: view.topAnchor, constant: self.navigationController?.navigationBar.frame.height ?? 0).isActive = true
-        headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        headerView.heightAnchor.constraint(equalToConstant: headerViewHeight).isActive = true
-        
-        currentLocationLabel.centerYAnchor.constraint(equalTo: headerView.centerYAnchor, constant: 30).isActive = true
-        currentLocationLabel.leadingAnchor.constraint(equalTo: headerImageView.trailingAnchor, constant: 20).isActive = true
-        
-        separatorView.topAnchor.constraint(equalTo: headerView.bottomAnchor).isActive = true
-        separatorView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        separatorView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        separatorView.heightAnchor.constraint(equalToConstant: 5).isActive = true // Set height to 10 for the line
-        
-        tableView.topAnchor.constraint(equalTo: headerView.bottomAnchor).isActive = true
-        tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-    }
-    @objc func headerTapped() {
-
-        print("Header tapped")
-    }
-}
-// MARK: - UITableViewDataSource && UITableViewDelegate
-extension SearchViewController : UITableViewDataSource, UITableViewDelegate{
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return places.count // Number of rows in the table view
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return cellHeight // Height of each cell
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
-        var place : String?
-        if let cell = cell as? SearchBarTableViewCell {
-            place = places[indexPath.row].name // Assuming places holds your data source
-            var searchTableViewModel = SearchTableViewModel(locationName: place ?? "unknown place", distance: "11km")
-            cell.searchTableViewModel = searchTableViewModel
-            
-            
-        }
-        
-        return cell
-    }
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        
-        tableView.isHidden = true
-        
-        let place = places[indexPath.row]
-        GooglePlacesManeger.shared.resolveLocation(for: place) {[weak self] result in
-          
-            switch result {
-              
-          case .success(let coordinate ):
-            
-              DispatchQueue.main.async{
-              self?.delegate?.didTapPlaceWithCoordinate(with: coordinate)
-            }
-          case .failure(let error):
-            print(error)
-          }
-        }
-        
-    }
-}
-extension SearchViewController : UISearchBarDelegate{
+extension SearchViewController : UISearchBarDelegate {
+   
     internal func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         print("Search button clicked with text: \(searchBar.text ?? "")")
         places.removeAll()
@@ -196,5 +53,59 @@ extension SearchViewController : UISearchBarDelegate{
           }
         }
         tableView.reloadData()
+    }
+    
+    
+    
+}
+
+extension SearchViewController : UITableViewDelegate , UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data[section].count
+
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "searchCell", for: indexPath) as! SearchTableViewCell
+        cell.handleCell(model: data[indexPath.section][indexPath.row])
+        
+        
+        
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        2
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+     return  section == 0 ? "" : "Geçmiş"
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        let headerView = (view as! UITableViewHeaderFooterView)
+        headerView.textLabel?.font.withSize(60)
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        tableView.isHidden = true
+        
+      /*  let place = places[indexPath.row]
+        GooglePlacesManeger.shared.resolveLocation(for: place) {[weak self] result in
+          
+            switch result {
+              
+          case .success(let coordinate ):
+            
+              DispatchQueue.main.async{
+              self?.delegate?.didTapPlaceWithCoordinate(with: coordinate)
+            }
+          case .failure(let error):
+            print(error)
+          }
+        }*/
+        
     }
 }
